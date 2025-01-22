aca292d4bf21aad91dd46ad62dd19faa6c299b22
Damian antony <jemcydami55@gmail.com>
2025-01-23 00:35:59 +0530

    feat:add functionality to product session,update some styles, add some elements

--- script.js
+++ script.js
@@ -1,9 +1,116 @@
 const slider=document.querySelector(".sliderWrapper");
 const menuOption= document.querySelectorAll('.menuitem');
 
+const product=[
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./images/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./images/air2.png",
+      },
+    ],
+  },
+  {    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./images/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./images/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./images/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./images/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./images/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./images/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./images/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./images/hippie2.png",
+      },
+    ],
+  }
+];
+
+let choosenProduct = product[0];
+const choosenProductImg=document.querySelector('.productimg');
+const choosenProductTitle=document.querySelector(".productTitle");
+const chooseProductPrice=document.querySelector(".productPrice");
+const chooseProductColor=document.querySelectorAll('.color');
+const chooseProductSize=document.querySelectorAll('.size');
+console.log(choosenProduct);
 menuOption.forEach((slides, index)=>{
   slides.addEventListener("click",()=>{
+    //slide changing 
     slider.style.transform=`translateX(${-100 * index}vw)`
+
+  // change product
+  choosenProduct=product[index];
+
+  // change product session contents
+  choosenProductTitle.textContent= choosenProduct.title;
+  chooseProductPrice.textContent=`$${choosenProduct.price}`
+  
+  choosenProductImg.src=choosenProduct.colors[0].img;
+  chooseProductColor.forEach((color, index)=>{
+    color.style.backgroundColor=choosenProduct.colors[index].code;
+
   })
 
+
+
+  })
+
+})
+chooseProductColor.forEach((color,index)=>{
+  color.addEventListener('click',(event)=>{
+    console.log(event);
+    choosenProductImg.src=choosenProduct.colors[index].img;
+  })
 })
